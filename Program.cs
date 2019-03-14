3fc467fc5904df5e04f1218e98ef0bef5f1c9ac0
bergtr <43316384+bergtr@users.noreply.github.com>
2019-03-14 17:09:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;	
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(IPAddress.Any, 8080);
+            Byte[] senddata = Encoding.ASCII.GetBytes("Hello World");
+            udpClient.Send(senddata, senddata.Length);
+        }
+    }
+}
